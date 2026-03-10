b653d8cf505024d2b3ed454ea13d0b19abbe40ef
nareum <ilu52046224@gmail.com>
2026-03-10 22:06:58 +0900

    fix: iOS weather permission + safe area + social login

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,13 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'kr.co.barusa.barupick',
   appName: '바루픽',
-  webDir: 'dist'
+  webDir: 'dist',
+  ios: {
+    contentInset: 'always',
+  },
+  server: {
+    iosScheme: 'capacitor'
+  }
 };
 
 export default config;
